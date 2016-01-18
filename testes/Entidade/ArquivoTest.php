caf76609b7074db3c2ade046973a06ef89674370
Alex Moreno <alex.moreno.costa@gmail.com>
2016-01-18 21:54:08 -0200

    Classe modificada para ser uma extensão de SplFileInfo. Reescrito metodo rename e getFilename Criado metodo delete

--- testes/Entidade/ArquivoTest.php
+++ testes/Entidade/ArquivoTest.php
@@ -7,20 +7,23 @@ use Alex\Entidade\Arquivo;
 
 class ArquivoTest extends PHPUnit
 {
+    protected function criarArquivo($filename)
+    {
+        $arquivo = __DIR__ . '/../arquivos/' . $filename;
+        $handle = fopen($arquivo, 'w+');
+        fclose($handle);
+        return $arquivo;
+    }
+    
     public function testCriarArquivo()
     {
-        $filename = __DIR__ . '/../arquivos/teste.txt';
-        $handle = fopen($filename, 'w+');
-        fclose($handle);
+        $filename = $this->criarArquivo('arquivo.txt');
         unlink($filename);
     }
     
     public function testVerificaSeAClasseReconheceOArquivo()
     {
-        $arquivo = __DIR__ . '/../arquivos/arquivo.txt';
-        $handle = fopen($arquivo, 'w+');
-        fwrite($handle, 'conteudo');
-        fclose($handle);
+        $arquivo = $this->criarArquivo('arquivo.txt');
         
         $file = new Arquivo($arquivo);
         $this->assertInstanceOf(
@@ -29,48 +32,40 @@ class ArquivoTest extends PHPUnit
                 sprintf('O caminho informado (%s) não é un arquivo', $arquivo)
                 );
         
-        $this->assertEquals($file->getNome(), 'arquivo', sprintf('O nome do arquivo não bateu, %s', $file->getNome()));
-        $this->assertEquals($file->getExtensao(), 'txt', sprintf('A extensão do arquivo não bateu, %s', $file->getExtensao()));
-        $this->assertEquals($file->getMimeType(), 'text/plain', sprintf('O mimeType do arquivo não bateu, %s', $file->getMimeType()));
-        $this->assertEquals($file->getDiretorio(), __DIR__ . '/../arquivos', sprintf('O caminho do arquivo não bateu, %s', $file->getDiretorio()));
-        $this->assertEquals($file->getNomeBase(), 'arquivo.txt', sprintf('O nome de base do arquivo não bateu, %s', $file->getNomeBase()));
-        $this->assertEquals($file->getCaminhoCompleto(), realpath($arquivo), 'O caminho completo do arquivo não bateu');
+        $this->assertEquals('arquivo', $file->getFilename());
+        $this->assertEquals('txt', $file->getExtension());
+        $this->assertEquals('arquivo.txt', $file->getBasename());
+        $this->assertEquals(__DIR__ . '/../arquivos', $file->getPath());
         unlink($arquivo);
     }
     
-    public function testRenomearArquivo()
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testNaoDeveReconhecerComoUmArquivoValido()
     {
-        $filename = __DIR__ . '/../arquivos/arquivo.txt';
-        $handle = fopen($filename, 'w+');
-        fclose($handle);
-        
-        $arquivo = new Arquivo($filename);
-        
-        $this->assertEquals($arquivo->getNome(), 'arquivo', sprintf('O nome do arquivo não bateu, %s', $arquivo->getNome()));
-        $this->assertEquals($arquivo->getExtensao(), 'txt', sprintf('A extensão do arquivo não bateu, %s', $arquivo->getExtensao()));
-        
-        $arquivo->renomear('modificado');
-        $this->assertEquals($arquivo->getNome(), 'modificado', sprintf('O nome do arquivo não bateu, %s', $arquivo->getNome()));
-        $this->assertEquals($arquivo->getExtensao(), 'txt', sprintf('A extensão do arquivo não bateu, %s', $arquivo->getExtensao()));
-        
-        unlink($arquivo->getCaminhoCompleto());
+        $filename = __DIR__;
+        $file = new Arquivo($filename);
     }
     
-    public function testAlterarExtensaoDoArquivo()
+    public function testRenameFile()
     {
-        $filename = __DIR__ . '/../arquivos/arquivo.txt';
-        $handle = fopen($filename, 'w+');
-        fclose($handle);
-        
-        $arquivo = new Arquivo($filename);
-        
-        $this->assertEquals($arquivo->getNome(), 'arquivo', sprintf('O nome do arquivo não bateu, %s', $arquivo->getNome()));
-        $this->assertEquals($arquivo->getExtensao(), 'txt', sprintf('A extensão do arquivo não bateu, %s', $arquivo->getExtensao()));
-        
-        $arquivo->alterarExtensao('html');
-        $this->assertEquals($arquivo->getNome(), 'arquivo', sprintf('O nome do arquivo não bateu, %s', $arquivo->getNome()));
-        $this->assertEquals($arquivo->getExtensao(), 'html', sprintf('A extensão do arquivo não bateu, %s', $arquivo->getExtensao()));
-        
-        unlink($arquivo->getCaminhoCompleto());
+        $filename = $this->criarArquivo('arquivo.txt');
+        $file = new Arquivo($filename);
+        $file->rename('modificado');
+        $this->assertEquals('modificado', $file->getFilename());
+        unlink($file->getRealPath());
+    }
+    
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testDelete()
+    {
+        $filename = $this->criarArquivo('arquivo.txt');
+        $file = new Arquivo($filename);
+        $this->assertTrue($file->delete());
+        unset($file);
+        $file = new Arquivo($filename);
     }
 }
